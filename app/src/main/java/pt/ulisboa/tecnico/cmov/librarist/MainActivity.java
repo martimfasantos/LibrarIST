5ddafcccd77ea705231d6259a75042d5b2e69a1c
martimfasantos <marty.f.a.santos@gmail.com>
2023-05-17 18:04:39 +0100

    more map features, user location done

--- app/src/main/java/pt/ulisboa/tecnico/cmov/librarist/MainActivity.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/librarist/MainActivity.java
@@ -1,15 +1,27 @@
 package pt.ulisboa.tecnico.cmov.librarist;
 
 import android.content.Intent;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -17,10 +29,35 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.libraries.places.api.Places;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+
+    // The entry point to the Fused Location Provider.
+    private FusedLocationProviderClient fusedLocationProviderClient;
+
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    private boolean locationPermissionGranted;
+    // A default location (Lisbon, Portugal) and default zoom to use when location permission is
+    // not granted.
+    private final LatLng defaultLocation = new LatLng(38.736946, -9.142685);
+    private static final int DEFAULT_ZOOM = 17;
+
+    // The geographical location where the device is currently located. That is, the last-known
+    // location retrieved by the Fused Location Provider.
+    private Location lastKnownLocation;
+
+    // Keys for storing activity state.
+    private static final String KEY_CAMERA_POSITION = "camera_position";
+    private static final String KEY_LOCATION = "location";
     public final static String EXTRA_MESSAGE = "pt.ulisboa.tecnico.cmov.librarist.MESSAGE";
 
     @Override
@@ -29,15 +66,16 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         setContentView(R.layout.activity_main);
         Log.d("MainActivity", "Hey, here is my fancy debug message!");
 
-        // Start Maps Activity (TODO verify if this can be done -- if it is preferable to remove the
-        //  TODO -- maps activity and put in on the main activity */
+        // Construct a PlacesClient
+        Places.initialize(getApplicationContext(), getString(R.string.maps_api_key));
+        // The entry point to the Places API.
+        Places.createClient(this);
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        // Construct a FusedLocationProviderClient.
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
 
-        Toast.makeText(getApplicationContext(), "Map loaded in Sydney!", Toast.LENGTH_SHORT).show();
+        // Initializes the map
+        initMap();
 
         Button library_info_btn = (Button) findViewById(R.id.library_info_btn);
         library_info_btn.setOnClickListener(new View.OnClickListener() {
@@ -57,9 +95,33 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             public void onClick(View view) {
                 EditText editText = (EditText) findViewById(R.id.location_input);
                 String location = editText.getText().toString();
-                goToLocationMap(location);
+
+                Geocoder geocoder = new Geocoder(MainActivity.this, Locale.getDefault());
+                try {
+                    // Get location coordinates that best match the given name
+                    List<Address> addressList = geocoder.getFromLocationName(location, 1);
+                    if (!addressList.isEmpty()){
+                        Address address = addressList.get(0);
+                        goToLocation(new LatLng(address.getLatitude(), address.getLongitude()));
+
+                        Toast.makeText(MainActivity.this, "Centered in" + address.getLocality(), Toast.LENGTH_SHORT).show();
+                    }
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+
             }
         });
+    }
+
+    private void initMap(){
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        assert mapFragment != null;
+        mapFragment.getMapAsync(this);
+
+        Toast.makeText(getApplicationContext(), "Map loaded in current location!", Toast.LENGTH_SHORT).show();
     }
 
     /**
@@ -73,48 +135,140 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
      * installed Google Play services and returned to the app.
      */
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onMapReady(@NonNull GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Set the center coordinates
-        LatLng coordinates = new LatLng(-34, 151);
+        // Turn on the My Location layer and the related control on the map.
+        updateLocationUI();
+
+        // Get the current location of the device and set the position of the map.
+        getDeviceLocation();
+    }
+
+    private void goToLocation(LatLng coordinates) {
 
         // Create a CameraPosition with desired properties
         CameraPosition cameraPosition = new CameraPosition.Builder()
                 .target(coordinates)
-                .zoom(12)
+                .zoom(DEFAULT_ZOOM)
                 .build();
 
-        // Add a marker in to your coordinates and move the camera smoothly
-        // TODO put this in the user's coordinates (with his/her permission)
+        // Add a marker in desired location and move the camera smoothly
         mMap.addMarker(new MarkerOptions()
                 .position(coordinates)
-                .title("You are here!"));
-
+                .title("Name of location"));
         // Animate the camera movement
         mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-
     }
 
-    public void goToLocationMap(String location){
-        if (location.equals("Lisboa")){
+    private void getLocationPermission() {
+        /*
+         * Request location permission, so that we can get the location of the
+         * device. The result of the permission request is handled by a callback,
+         * onRequestPermissionsResult.
+         */
+        ActivityCompat.requestPermissions(this,
+                new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+    }
 
-            LatLng coordinates = new LatLng(38.736946, -9.142685);
-
-            // Create a CameraPosition with desired properties
-            CameraPosition cameraPosition = new CameraPosition.Builder()
-                    .target(coordinates)
-                    .zoom(10)
-                    .build();
-
-            // Add a marker in desired location and move the camera smoothly
-            // TODO put this in the user's location (with his/her permission)
-            mMap.addMarker(new MarkerOptions()
-                    .position(coordinates)
-                    .title("Name of location"));
-            // Animate the camera movement
-            mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        locationPermissionGranted = false;
+        if (requestCode == PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION) {// If request is cancelled, the result arrays are empty.
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                locationPermissionGranted = true;
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         }
+
+        // Update Location with the new permissions
+        updateLocationUI();
+        // Update current location of the device
+        getDeviceLocation();
+    }
+
+    private void updateLocationUI() {
+        if (mMap == null) { return; }
+        try {
+            // Check if Permission is granted
+            if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                    android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                locationPermissionGranted = true;
+            }
+
+            Log.d("Location Permission Granted", String.valueOf(locationPermissionGranted));
+
+            if (locationPermissionGranted) {
+                mMap.setMyLocationEnabled(true);
+                mMap.getUiSettings().setMyLocationButtonEnabled(true);
+            } else {
+                mMap.setMyLocationEnabled(false);
+                mMap.getUiSettings().setMyLocationButtonEnabled(false);
+                lastKnownLocation = null;
+                getLocationPermission();
+            }
+        } catch (SecurityException e)  {
+            Log.e("Exception: %s", e.getMessage());
+        }
+    }
+
+    private void getDeviceLocation() {
+        /*
+         * Get the best and most recent location of the device, which may be null in rare
+         * cases when a location is not available.
+         */
+        try {
+            if (locationPermissionGranted) {
+                // TODO check if information is outdated with the getLastLocation()
+                Task<Location> locationResult = fusedLocationProviderClient.getLastLocation();
+                locationResult.addOnCompleteListener(this, new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Location> task) {
+                        if (task.isSuccessful()) {
+                            // Set the map's camera position to the current location of the device.
+                            lastKnownLocation = task.getResult();
+                            if (lastKnownLocation != null) {
+                                mMap.moveCamera(CameraUpdateFactory
+                                        .newLatLngZoom(new LatLng(lastKnownLocation.getLatitude(),
+                                                lastKnownLocation.getLongitude()), DEFAULT_ZOOM));
+                            }
+
+                        // If for some reason Current location is Null, we use the defaults
+                        } else {
+                            Log.d(EXTRA_MESSAGE, "Current location is null. Using defaults.");
+                            Log.e(EXTRA_MESSAGE, "Exception: %s", task.getException());
+                            mMap.moveCamera(CameraUpdateFactory
+                                    .newLatLngZoom(defaultLocation, DEFAULT_ZOOM-5));
+                            mMap.getUiSettings().setMyLocationButtonEnabled(false);
+
+                            Toast.makeText(MainActivity.this, "Current location is null", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+            // If permission is not granted, move camera to the default Location
+            } else {
+                mMap.moveCamera(CameraUpdateFactory
+                        .newLatLngZoom(defaultLocation, DEFAULT_ZOOM-5));
+            }
+        } catch (SecurityException e) {
+            Log.e("Exception: %s", e.getMessage(), e);
+        }
+    }
+
+    // Save the current map (location and camera position)
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        if (mMap != null) {
+            outState.putParcelable(KEY_CAMERA_POSITION, mMap.getCameraPosition());
+            outState.putParcelable(KEY_LOCATION, lastKnownLocation);
+        }
+        super.onSaveInstanceState(outState);
     }
 
     // Start the service
